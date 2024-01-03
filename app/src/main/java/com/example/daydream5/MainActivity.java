b9526b5ea4ef12d292add98445761fc63aa11210
neil2ball <neil2ball@gmail.com>
2024-01-03 12:56:13 -0500

    Add QR code maker draft; miscellaneous updates

--- app/src/main/java/com/example/daydream5/MainActivity.java
+++ app/src/main/java/com/example/daydream5/MainActivity.java
@@ -3,6 +3,23 @@ package com.example.daydream5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Switch;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+
+import android.text.TextUtils;
+
+import android.util.Log;
+
+import android.view.View;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,24 +31,134 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView qrCodeIV;
+    private RadioButton middayRadBtn;
+    private RadioButton eveningRadBtn;
+    private RadioButton bothRadBtn;
+    private Switch ezMatchSwitch;
+
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+
+    private EditText dataEdtDays;
+    private EditText dataEdtPlays;
+
     List<List<Byte[]>> fantasy5Numbers = new ArrayList<>();
     List<Byte> numbersList = new ArrayList<>();
     List<List<Boolean>> pickTrackerList = new ArrayList<>();
     List<String> pickStrings = new ArrayList<>();
+    String prefixString;
+    List<String> playStrings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        numbersList = shuffleByteList();
-
-        createPickTracker(pickTrackerList);
-
-        fantasy5Numbers = csvReader("file.csv");
-
-        sortPicks();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrCodeIV = findViewById(R.id.idIVQrcode);
+
+        Button generateQrBtn = findViewById(R.id.idBtnGenerateQR);
+
+        middayRadBtn = findViewById(R.id.radioButtonMidday);
+
+        eveningRadBtn = findViewById(R.id.radioButtonEvening);
+
+        bothRadBtn = findViewById(R.id.radioButtonBoth);
+
+        ezMatchSwitch = findViewById(R.id.switchEzMatch);
+
+        dataEdtDays = findViewById(R.id.editTextNumberDays);
+
+        dataEdtPlays = findViewById(R.id.editTextNumberPlays);
+
+        //https://www.geeksforgeeks.org/how-to-generate-qr-code-in-android/
+        // initializing onclick listener for button.
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                numbersList = shuffleByteList();
+
+                createPickTracker(pickTrackerList);
+
+                fantasy5Numbers = csvReader("file.csv");
+
+                int dayAmount = 1;
+                int playAmount = 1;
+
+                try {
+                    if (TextUtils.isEmpty(dataEdtDays.getText().toString()) || TextUtils.isEmpty(dataEdtPlays.getText().toString())) {
+
+                        if (TextUtils.isEmpty(dataEdtDays.getText().toString())) {
+                            dataEdtDays.setText("1");
+                        }
+                        if (TextUtils.isEmpty(dataEdtPlays.getText().toString())) {
+                            dataEdtPlays.setText("1");
+                        }
+
+                    }
+
+                    dayAmount = Integer.parseInt(dataEdtDays.getText().toString());
+                    if (dayAmount < 1) {
+                        dayAmount = 1;
+                    }
+                    else if (dayAmount > 30) {
+                        dayAmount = 30;
+                    }
+
+                    playAmount = Integer.parseInt(dataEdtPlays.getText().toString());
+
+                    if (playAmount < 1) {
+                        playAmount = 1;
+                    }
+                    else if (playAmount > fantasy5Numbers.size()) {
+                        playAmount = fantasy5Numbers.size();
+                    }
+                }
+                catch (Exception e) {
+                    dayAmount = 1;
+                    playAmount = 1;
+                }
+
+                StringBuilder pickPrefix = new StringBuilder();
+
+                pickPrefix.append("lot40:WFD");
+
+                pickPrefix.append(String.format(Locale.US, "%02d", dayAmount));
+
+                pickPrefix.append("T");
+
+                if (middayRadBtn.isChecked()) {
+                    pickPrefix.append("M");
+                }
+                else if (eveningRadBtn.isChecked()) {
+                    pickPrefix.append("E");
+                }
+                else if (bothRadBtn.isChecked()) {
+                    pickPrefix.append("B");
+                }
+
+                pickPrefix.append("E");
+
+                if (ezMatchSwitch.isChecked()) {
+                    pickPrefix.append("Y");
+                }
+                else {
+                    pickPrefix.append("N");
+                }
+
+                pickPrefix.append("S");
+
+                prefixString = pickPrefix.toString();
+
+                sortPicks(playAmount);
+
+                qrStringMaker();
+
+                qrMaker(qrCodeIV, playStrings.get(0));
+
+            }
+        });
     }
 
     //https://www.stackchief.com/blog/How%20to%20read%20a%20CSV%20file%20in%20Java%20%7C%20with%20examples
@@ -90,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
             Collections.shuffle(listsList);
 
         } catch (Exception e){
-
+            Log.e("csvReader", e.toString());
         }
         return listsList;
     }
@@ -120,16 +247,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void sortPicks () {
+    protected void sortPicks (int playCount) {
 
-        while (fantasy5Numbers.size() > 0) {
+        while (fantasy5Numbers.size() > 0 && playCount > 0) {
             for (byte w = 0; w < fantasy5Numbers.size(); w++) { //maximum of 4 lists. We rotate through these lists to evenly distribute the picks among the colored groupings.
-                boolean breakFlag = false; //we must break out of the nested loops when we make a pick.
-                for (int x = 0; x < fantasy5Numbers.get(w).size(); x++) { //maximum 26244 picks per list
+                if (playCount > 0) {
+                    boolean breakFlag = false; //we must break out of the nested loops when we make a pick.
+                    for (int x = 0; x < fantasy5Numbers.get(w).size(); x++) { //maximum 26244 picks per list
 
                         for (byte y = 0; y < pickTrackerList.get(0).size(); y++) { //maximum 36 numbers; tracks whether a specific number was used (in the first [of five] lists)
                             if (pickTrackerList.get(0).get(y)) { //Use the first list to find what numbers we have not used yet; the attempt is to create a lottery wheel.
-                                for (byte z=0; z < fantasy5Numbers.get(w).get(x).length; z++) { //max of 5 numbers per pick
+                                for (byte z = 0; z < fantasy5Numbers.get(w).get(x).length; z++) { //max of 5 numbers per pick
 
                                     if (numbersList.get(y).equals(fantasy5Numbers.get(w).get(x)[z])) { //if the needed number is contained in the pick
                                         //We need to know the address of each pick. That way we can randomize each number in the picks and keep track of their uses.
@@ -178,7 +306,7 @@ public class MainActivity extends AppCompatActivity {
                                                 //https://stackoverflow.com/questions/48872155/string-concatenation-in-loop
                                                 String pickNumberString;
                                                 for (byte f = 0; f < fantasy5Numbers.get(w).get(x).length; f++) {
-                                                    pickNumberString = String.format(Locale.US,"%02d",fantasy5Numbers.get(w).get(x)[f]); //We need to make the bytes two digits no matter what.
+                                                    pickNumberString = String.format(Locale.US, "%02d", fantasy5Numbers.get(w).get(x)[f]); //We need to make the bytes two digits no matter what.
                                                     //We start with bytes to attempt to save memory.
 
                                                     pickString.append(pickNumberString); //append the values as a string
@@ -187,9 +315,9 @@ public class MainActivity extends AppCompatActivity {
 
                                                 pickStrings.add(pickString.toString()); //add the string of the numbers in the pick to the list of picks
                                                 fantasy5Numbers.get(w).remove(x); //remove the pick from the list of picks. This is how we keep track of when to stop using the lists.
-
+                                                playCount--;
                                                 if (fantasy5Numbers.get(w).size() == 0) {
-                                                        fantasy5Numbers.remove(w); //remove one of the four list groups if there are no more numbers in that list.
+                                                    fantasy5Numbers.remove(w); //remove one of the four list groups if there are no more numbers in that list.
                                                 }
 
                                                 for (byte h = 0; h < picksIndex.length; h++) {
@@ -199,9 +327,8 @@ public class MainActivity extends AppCompatActivity {
                                                 breakFlag = true;
 
                                                 break; //break out of this loop so that we can break out of the outer loops.
-                                            }
-                                            catch (Exception e) {
-
+                                            } catch (Exception e) {
+                                                Log.e("sortPicks", e.toString());
                                             }
                                         }
                                     }
@@ -210,8 +337,7 @@ public class MainActivity extends AppCompatActivity {
                                 if (breakFlag) {
                                     break; //break out of this loop so that we can break out of the outer loop.
                                 }
-                            }
-                            else if (y == pickTrackerList.get(0).size() - 1) { //This is used when there are no remaining picks in the first pick tracker list.
+                            } else if (y == pickTrackerList.get(0).size() - 1) { //This is used when there are no remaining picks in the first pick tracker list.
                                 List<List<Boolean>> pickTrackerBuffer = new ArrayList<>();
 
                                 createPickTracker(pickTrackerBuffer); //This creates a fresh tracker list.
@@ -224,12 +350,70 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
 
-                    if(breakFlag) {
-                        break; //break out of this loop so that we can move on to the next list.
+                        if (breakFlag) {
+                            break; //break out of this loop so that we can move on to the next list.
+                        }
                     }
+                }
+                else {
+                    break;
                 }
             }
         }
+        fantasy5Numbers.clear();
     }
 
+    protected void qrMaker (ImageView qrCodeIV, String dataEdt) {
+        // creating a variable for point which
+        // is to be displayed in QR Code.
+        Point point = new Point();
+
+        // getting width and
+        // height of a point
+        int width = point.x;
+        int height = point.y;
+
+        // generating dimension from width and height.
+        int dimen = Math.min(width, height);
+        dimen = dimen * 3 / 4;
+
+        // setting this dimensions inside our qr code
+        // encoder to generate our qr code.
+        qrgEncoder = new QRGEncoder(dataEdt, null, QRGContents.Type.TEXT, dimen);
+        try {
+            // getting our qrcode in the form of bitmap.
+            bitmap = qrgEncoder.getBitmap();
+            // the bitmap is set inside our image
+            // view using .setimagebitmap method.
+            qrCodeIV.setImageBitmap(bitmap);
+        }
+        catch (Exception e) {
+            // this method is called for
+            // exception handling.
+            Log.e("qrMaker", e.toString());
+        }
+    }
+
+    protected void qrStringMaker () {
+
+        int pickStringsPlayslipCount = pickStrings.size() / 10;
+        for (int x = 0; x < pickStringsPlayslipCount; x++) {
+            StringBuilder playStringBuilder = new StringBuilder();
+            playStringBuilder.append(prefixString);
+            for (int y = 0; y < 10; y++) {
+                playStringBuilder.append(pickStrings.get(y));
+                pickStrings.remove(y);
+            }
+            playStrings.add(playStringBuilder.toString());
+        }
+        if (pickStrings.size() % 10 != 0) {
+            StringBuilder playStringBuilder = new StringBuilder();
+            playStringBuilder.append(prefixString);
+            for (int y = 0; y < pickStrings.size(); y++) {
+                playStringBuilder.append(pickStrings.get(y));
+                pickStrings.remove(y);
+            }
+            playStrings.add(playStringBuilder.toString());
+        }
+    }
 }
